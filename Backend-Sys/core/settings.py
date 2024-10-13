12ce939b838a5e7e6ec39bb0978b38e3332979e2
Dan-njuguna <njugush020@gmail.com>
2024-10-13 22:58:57 +0300

    Changes: environment configuration file loading

--- Backend-Sys/core/settings.py
+++ Backend-Sys/core/settings.py
@@ -11,16 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n*5hqnvmwrrwn(z4!)6=i()cp(#==zj=ndjwm_6i2kf-erhptv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'consumers',
 ]
 
 MIDDLEWARE = [
