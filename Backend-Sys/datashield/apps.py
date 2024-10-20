d39328f52ab8272ddff58046a76c0a7f6ba547d6
SusanGicheru07 <susangicheru07@gmail.com>
2024-10-20 18:52:58 +0300

    Restructured the directories to avoid merging conflicts

--- Backend-Sys/datashield/apps.py
+++ Backend-Sys/datashield/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TestBackendAppConfig(AppConfig):
+class DatashieldConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'test_backend_app'
+    name = 'datashield'
