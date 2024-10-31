4f617ff3c45c2377f6a0ee11d918a2d170ef02d2
Susan Gicheru <139166838+SusanGicheru07@users.noreply.github.com>
2024-10-31 16:12:05 +0300

    Delete audit_log directory

--- audit_log/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import AuditLog
-
-class AuditLogForm(forms.ModelForm):
-
-    class Meta:
-        model = AuditLog
-        fields = ["organization","user","action","action_description","affected_resource","ip_address","is_compliant","compliance_notes","additional_data"]
-
