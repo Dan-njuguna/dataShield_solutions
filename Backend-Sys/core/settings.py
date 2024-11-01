85533a00d15e21b7101e916cc84e624681897104
Dan-njuguna <njugush020@gmail.com>
2024-11-01 18:16:19 +0300

    Refactor settings.py: reorganize imports, update template directories, and enhance database configuration

--- Backend-Sys/core/settings.py
+++ Backend-Sys/core/settings.py
@@ -2,31 +2,36 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
+
 DBUSER = os.getenv("DBUSER")
 DBNAME = os.getenv("DBNAME")
 DBPASS = os.getenv("DBPASS")
 DBHOST = os.getenv("DBHOST")
 DBPORT = os.getenv("DBPORT")
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 SECRET_KEY = os.getenv("SECRET_KEY")
+
 DEBUG = True
 ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
- "django.contrib.admin",
- "django.contrib.auth",
- "django.contrib.contenttypes",
- "django.contrib.sessions",
- "django.contrib.messages",
- "django.contrib.staticfiles",
     "analytics",
     "policy_management",
     "authentication",
     "compliance_management",
     "reporting",
     "audit_log",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
  ]
 
 
@@ -41,32 +46,48 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "core.urls"
 TEMPLATES = [
- {'BACKEND':"django.template.backends.django.DjangoTemplates", 
-  'DIRS':[],  'APP_DIRS':True, 
-  'OPTIONS':{"context_processors": [
-                          "django.template.context_processors.debug",
-                          "django.template.context_processors.request",
-                          "django.contrib.auth.context_processors.auth",
-                          "django.contrib.messages.context_processors.messages"]}}]
+    {'BACKEND':"django.template.backends.django.DjangoTemplates", 
+    'DIRS': ["../../Frontend-Sys/ds-react"],
+    'APP_DIRS':True,
+    'OPTIONS':{
+        "context_processors": [
+            "django.template.context_processors.debug",
+            "django.template.context_processors.request",
+            "django.contrib.auth.context_processors.auth",
+            "django.contrib.messages.context_processors.messages"
+            ]
+        }
+    }
+]
 WSGI_APPLICATION = "core.wsgi.application"
-DATABASES = {"default": {
+DATABASES = {
+    "default": {
               'ENGINE': "django.db.backends.postgresql",
               'NAME': DBNAME,
               'USER': DBUSER,
               'PASSWORD': DBPASS,
               'HOST': DBHOST,
-              'PORT': DBPORT}}
+              'PORT': DBPORT
+              }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
- {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
- {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
- {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
- {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"}]
+        {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+        {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+        {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+        {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"}
+    ]
+
+
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
+
 STATIC_URL = "static/"
+STATICFILES_DIRS = [Path(__file__).resolve().parent.parent.parent, "staticfiles"]
+STATIC_ROOT = ("../../Frontend-Sys/ds-react/src")
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 ASGI_APPLICATION = 'core.asgi.application' 
