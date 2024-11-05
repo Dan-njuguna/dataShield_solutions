dce1787defae890b8ff3c64f86e864aea999c4eb
Dan-njuguna <njugush020@gmail.com>
2024-11-05 22:56:25 +0300

    Add text tokenization function and label encoding for classification model

--- Model/ClassificationModels/model.py
+++ Model/ClassificationModels/model.py
@@ -5,14 +5,27 @@ import torch
 import transformers
 from transformers import BertTokenizer, BertModel, AutoTokenizer, AutoModelForSequenceClassification
 
-
-# DATASET PATH
-DATASET_PATH = "../Datasets/"
-
-gdpr_path = DATASET_PATH + "gdpr4.csv"
-gdpr = pd.read_csv(gdpr_path)
-
 # MODEL PATH
 MODEL_PATH = "../TrainedModels/"
 
 # BERT MODEL
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+def tokenize_text_data(text):
+    '''Tokenizes all text data in a specified column of a DataFrame
+
+    - df: pd.DataFrame
+    - column_name: str
+    Return DataFrame with tokenized text
+    '''
+    inputs = tokenizer(text, return_tensors='pt', padding=True, truncation=True, max_length=512)
+    with torch.no_grad():
+        outputs = model(**inputs)
+    return outputs.last_hidden_state.mean(dim=1).squeeze().numpy()
+
+# Decode the output variables
+label_encoders = {}
+for column in ['Category', 'Description', 'Section in Act', 'Key Points']:
+    le = LabelEncoder()
+    kenya_train.loc[:, column] = le.fit_transform(kenya_train[column])
+    label_encoders[column] = le
